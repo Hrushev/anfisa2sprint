797223a5559c0f48e30bb1dc231ad9fba8c725ce
Denis Hrushev <super.hrushev2@mail.ru>
2023-08-14 11:28:17 +0300

    Complete

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 
+from ice_cream.models import IceCream
+
 
 def index(request):
-    template = 'homepage/index.html'
-    return render(request, template)
+    template_name = 'homepage/index.html'
+    ice_cream_list = IceCream.objects.select_related(
+        'category'
+        ).filter(
+        is_published=True,
+        category__is_published=True
+        ).order_by(
+        'category'
+        )
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    return render(request, template_name, context)
